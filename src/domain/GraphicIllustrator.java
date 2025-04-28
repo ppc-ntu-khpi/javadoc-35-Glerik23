cb3a70850e528622cf41ad15abc541b090c42c3a
Glerik <glerik1337@gmail.com>
2025-04-29 01:43:43 +0300

    Add generated Javadoc documentation

--- src/domain/GraphicIllustrator.java
+++ src/domain/GraphicIllustrator.java
@@ -1,3 +1,16 @@
 package domain;
+
+/**
+ * Представляє співробітника, який є графічним ілюстратором (Graphic Illustrator).
+ * Успадковується від {@link Artist}.
+ *
+ * @author Glerik
+ * @version 1.0
+ * @since 29.04.25
+ * @see Artist
+ * @see Employee
+ */
 public class GraphicIllustrator extends Artist {
+    // Цей клас успадковує всі властивості та методи від класу Artist.
+    // Якщо в майбутньому будуть додані специфічні поля або методи, їх потрібно буде задокументувати.
 }
